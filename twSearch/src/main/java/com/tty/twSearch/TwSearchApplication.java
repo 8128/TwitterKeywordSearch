2abc4b069f6dbd0f42a739868813ae08a5d5ff11
tty <tty8128@outlook.com>
2019-11-26 21:36:04 -0500

    update

---
+++ twSearch/src/main/java/com/tty/twSearch/TwSearchApplication.java
@@ -0,0 +1,13 @@
+package com.tty.twSearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwSearchApplication.class, args);
+	}
+
+}
---
+++ twSearch/src/test/java/com/tty/twSearch/TwSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tty.twSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
